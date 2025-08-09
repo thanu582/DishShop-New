6d28970a671f77c055b884af2d216b0a6a6428e4
thanu582 <141625045+thanu582@users.noreply.github.com>
2025-08-09 23:15:07 +0530

    Update settings.py

--- DishShop/settings.py
+++ DishShop/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key-here'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost'.'*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
